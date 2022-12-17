7260ea63a00b14a04e4f3cbc3272401378ec5e32
Kedar <kedarmakode1598@gmail.com>
2022-12-17 11:53:26 +0530

    initial commit

---
+++ src/reducer.js
@@ -0,0 +1,48 @@
+import {
+  SET_LOADING,
+  SET_STORIES,
+  REMOVE_STORY,
+  HANDLE_PAGE,
+  HANDLE_SEARCH,
+} from './actions'
+
+const reducer = (state, action) => {
+
+  switch(action.type){
+    case SET_LOADING:
+      return {...state, isLoading: true}
+
+    case SET_STORIES:
+      return {...state, isLoading: false, hits: action.payload.hits, nbPages: action.payload.nbPages}
+    
+    case REMOVE_STORY:
+      return {...state, hits: state.hits.filter((story) => story.objectID !== action.payload)}
+    
+    case HANDLE_SEARCH:
+      return {...state, query:action.payload, page: 0}
+
+    case HANDLE_PAGE:
+      if(action.payload === 'inc'){
+        let nextpage = state.page + 1
+        if(nextpage > state.nbPages - 1){
+          nextpage = 0
+        }
+        return {...state, page:nextpage}
+      }
+      
+      if(action.payload === 'dec'){
+        let prevpage = state.page + 1
+        if(prevpage < 0){
+          prevpage = 0
+        }
+        return {...state, page:prevpage}
+      }
+
+      
+    
+    default: 
+      throw new Error (`no matching ${action.type} action`)
+  }
+
+}
+export default reducer
